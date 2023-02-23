4ef6fb4687b569fb260a20250a97b2641915e680
Joseph Harold <jmharold@amazon.com>
2023-02-23 11:51:45 -0700

    First commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifymldemo33437a32": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "identifyTextdf933b8b": {
+      "format": "string",
+      "region": "string"
+    }
+  }
+}
